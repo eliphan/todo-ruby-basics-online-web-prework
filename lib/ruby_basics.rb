7e57cb47f0f9f9729274ea415096fec765622e7d
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-27 01:00:16 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -3,12 +3,11 @@ def division(num1, num2)
 end
 
 def assign_variable(name)
-  puts name
+  puts #{name}
 end
 
 def argue (ment)
   return ment
-  
 end
 
 def greeting (greet1, greet2)
@@ -17,15 +16,12 @@ end
 
 def return_a_value
   return "Nice"
-
 end
 
 def last_evaluated_value
   return "expert"
-
 end
 
 def pizza_party (arg = "cheese")
   return arg
-
 end
